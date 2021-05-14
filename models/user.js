c3bd74375ca35b3593b24c3699c49347b775c4e1
zviels <59054323+zviels@users.noreply.github.com>
2021-05-14 16:45:06 +0300

    Added Validations For The User Model

--- models/user.js
+++ models/user.js
@@ -20,10 +20,104 @@ module.exports = (sequelize, DataTypes) => {
 
   User.init({
 
-    firstName: DataTypes.STRING,
-    lastName: DataTypes.STRING,
-    emailAddress: DataTypes.STRING,
-    password: DataTypes.STRING
+    firstName: {
+
+      type: DataTypes.STRING,
+      allowNull: false,
+
+      validate: {
+
+        notNull: {
+
+          msg: 'First Name Is Required.'
+
+        },
+
+        notEmpty: {
+
+          msg: 'Please Provide A First Name.'
+
+        }
+
+      }
+
+    },
+
+    lastName: {
+
+      type: DataTypes.STRING,
+      allowNull: false,
+
+      validate: {
+
+        notNull: {
+
+          msg: 'Last Name Is Required.'
+
+        },
+
+        notEmpty: {
+
+          msg: 'Please Provide A Last Name.'
+
+        }
+
+      }
+
+    },
+
+    emailAddress: {
+
+      type: DataTypes.STRING,
+      allowNull: false,
+
+      unique: {
+
+        msg: 'The Email You Entered Already Exists.'
+
+      },
+
+      validate: {
+
+        notNull: {
+
+          msg: 'Email Address Is Required.'
+
+        },
+
+        isEmail: {
+
+          msg: 'Please Provide A Valid Email Address.'
+
+        }
+
+      }
+
+    },
+
+    password: {
+
+      type: DataTypes.STRING,
+      allowNull: false,
+
+      validate: {
+
+        notNull: {
+
+          msg: 'Password Is Required.'
+
+        },
+
+        len: {
+
+          args: [9, 40],
+          msg: 'The Password Should Be Between 9 And 40 Characters In Length.'
+
+        }
+
+      }
+      
+    }
 
   }, { sequelize, modelName: 'User' });
 
